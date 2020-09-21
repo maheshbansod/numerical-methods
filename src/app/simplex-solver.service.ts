e71c68026e363568c5ee9a5293d886a187afd544
Mahesh Bansod <mahesh0bansod@gmail.com>
2020-09-21 16:20:42 +0530

    handles =,<,> constraints

--- src/app/simplex-solver.service.ts
+++ src/app/simplex-solver.service.ts
@@ -7,15 +7,22 @@ export class SimplexSolverService {
 
   constructor() { }
 
-  costi = [8,5,0,0,0]; //Cost function coeffs
+  costi = [8,5]; //Cost function coeffs
   otype = "max"; //maximization or minimization problem
   status = 'idle';
 
   constmat = [
-              {type:'=',coeffs:[2,1,1,0,0],b:500 },
-              {type:'=',coeffs:[1,0,0,1,0],b:150 },
-              {type:'=',coeffs:[0,1,0,0,1],b:250}
+              {type:'<',coeffs:[2,1],b:500 },
+              {type:'<',coeffs:[1,0],b:150 },
+              {type:'<',coeffs:[0,1],b:250}
             ]; //constraint matrix
+
+  soff = -1; //slack variables offset
+  sn = 0; //no. of slack variables
+  aoff = -1; //artificial variables offset
+  an = 0; //no. of artificial variables
+  
+  bigM = 999999;
 
   set(otype, costi, constmat) {
     this.otype = otype;
@@ -39,10 +46,53 @@ export class SimplexSolverService {
     return this.status;
   }
 
+  addVariable(vtype) {
+    let off, n;
+    if(vtype == 's') {
+      off = this.soff;
+      n = this.sn++;
+    } else {
+      off = this.aoff;
+      n = this.an++;
+    }
+      if(off != -1) {
+        this.costi.splice(off+n,0,(vtype=='s')?0:this.bigM);
+        this.constmat.map( (c)=>c.coeffs.splice(off+n, 0,0) )
+      } else {
+        if(vtype == 's') {
+          this.soff = this.costi.length;
+          this.costi.push(0);
+        }
+        else {
+          this.aoff = this.costi.length;
+          this.costi.push(this.bigM);
+        }
+        this.constmat.map( (c)=>c.coeffs.push(0) );
+      }
+      return off+n-1;
+  }
+
   solve() {
     //solve for max problem with all = constraints only with slack variables already added.
     //TODO: convert problem by adding slack variables, etc and changing all constraints to = whenvever necessary
 
+    for(var i=0;i<this.constmat.length;i++) {
+      if(this.constmat[i].type == '<') { //add slack
+        let si = this.addVariable('s');
+        this.constmat[i].type = '=';
+        this.constmat[i].coeffs[si] = 1;
+      } else if(this.constmat[i].type == '>' || this.constmat[i].type == '=') { //add slack and artificial
+        let si = this.addVariable('s');
+        let ai = this.addVariable('a');
+        this.constmat[i].type = '=';
+        this.constmat[i].coeffs[ai] = this.constmat[i].coeffs[si] = 1;
+        if(this.constmat[i].type == '=') {
+          this.constmat.push({type:'<',coeffs: this.constmat[i].coeffs.map((x)=>x), b:this.constmat[i].b});
+        }
+      }
+    }
+    console.log(this.constmat);
+    console.log(this.costi);
     /*the last `n` variables are put in the basis first*/
     let basis = [...Array(this.constmat.length).keys()].map((x)=>x+this.costi.length - this.constmat.length);
     while(1) {
