27dcaa22a39c829a3afa71fd9648768228d3a654
Mahesh Bansod <mahesh0bansod@gmail.com>
2020-09-24 16:11:25 +0530

    add and remove for source and destination for transportation problem

--- src/app/transportation-solve/transportation-solve.component.ts
+++ src/app/transportation-solve/transportation-solve.component.ts
@@ -16,4 +16,27 @@ export class TransportationSolveComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  addSource() {
+    this.table.push(Array(this.demands.length).fill(0));
+    this.supplys.push(0);
+  }
+  addDestination() {
+    this.table.map( (x)=>x.push(0) );
+    this.demands.push(0);
+  }
+
+  removeSource() {
+    this.table.pop();
+    this.supplys.pop();
+  }
+
+  removeDestination() {
+    this.table.map( (x)=>x.pop() );
+    this.demands.pop();
+  }
+
+  trackBy(index: any, item: any) {
+    return index;
+  }
+
 }
