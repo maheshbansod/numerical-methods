61b8e0d83da32a85e7eb01b16cf2cbaec1613043
Mahesh Bansod <mahesh0bansod@gmail.com>
2020-09-25 13:59:15 +0530

    added least cost method

--- src/app/transportation-solve/transportation-solve.component.ts
+++ src/app/transportation-solve/transportation-solve.component.ts
@@ -19,13 +19,11 @@ export class TransportationSolveComponent implements OnInit {
   }
 
   findInitialSolution(method:String) {
+    let demands = this.demands.map( (x)=>[x] );
+    let supplys = this.supplys.map( (x)=>[x] );
+    let weights = [...Array(this.table.length)].map( (x)=>Array(demands.length).fill(0) );
     /**TODO: also check if problem is balanced */
-    if(method == 'nwcr') {
-      let demands = this.demands.map( (x)=>[x] );
-      let supplys = this.supplys.map( (x)=>[x] );
-      let weights = [...Array(this.table.length)].map( (x)=>Array(demands.length).fill(0) );
-
-      console.log(weights);
+    if(method == 'nwcr') { //north west corner rule
       let i=0, j=0;
       while( i<supplys.length && j<demands.length) {
         if(supplys[i][0] < demands[j][0]) {
@@ -42,8 +40,53 @@ export class TransportationSolveComponent implements OnInit {
       }
       this.solution = {weights:weights, supplys:supplys, demands:demands};
       this.showsolution = true;
-      console.log("done",this.solution);
+      //console.log("done",this.solution);
+    } else if(method == 'lcm') { //least cost method
+      let i=0,j=0,mini, minj, min=Infinity;
+      let table = this.table.map( (x)=>x.map(x=>x) );
+      while(1) {
+        min = Infinity;
+        for(i=0;i<table.length;i++) {
+          for(j=0;j<table[0].length;j++) {
+            if(table[i][j] < min) {
+              min = table[i][j];
+              mini = i;
+              minj = j;
+            }
+          }
+        }
+        if(!isFinite(min))
+          break;
+        i = mini;
+        j = minj;
+        if(supplys[i][0] < demands[j][0]) {
+          weights[i][j]=supplys[i][0];
+          demands[j].unshift(demands[j][0]-supplys[i][0]);
+          supplys[i].unshift(0);
+          for(i=0;i<table[0].length;i++)
+            table[mini][i] = Infinity;
+        } else {
+          weights[i][j] = demands[j][0];
+          supplys[i].unshift(supplys[i][0]-demands[j][0]);
+          demands[j].unshift(0);
+          for(i=0;i<table.length;i++)
+            table[i][minj] = Infinity;
+        }
+      }
+
+      this.solution = {weights:weights, supplys: supplys, demands:demands};
+      this.showsolution = true;
     }
+  }
+
+  getCost(table, weights) {
+    let cost = 0;
+    for(let i=0;i<table.length;i++) {
+      for(let j=0;j<table[0].length;j++) {
+        cost += table[i][j]*weights[i][j];
+      }
+    }
+    return cost;
   }
 
   addSource() {
