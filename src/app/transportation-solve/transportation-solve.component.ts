f12f0c1cb4dbc9a870e0dc25f46c8380d3b0dd10
Mahesh Bansod <mahesh0bansod@gmail.com>
2020-09-29 13:40:08 +0530

    added partial modi method

--- src/app/transportation-solve/transportation-solve.component.ts
+++ src/app/transportation-solve/transportation-solve.component.ts
@@ -10,13 +10,114 @@ export class TransportationSolveComponent implements OnInit {
 
   constructor() { }
 
-  table = [[2,7,4],[3,3,1],[5,4,7],[1,6,2]];
-  demands = [7,9,18]
-  supplys = [5,8,7,14];
+  table = [[5,4,2,6],[8,3,5,7],[5,9,4,6]];
+  demands = [10,40,20,30]
+  supplys = [20,30, 50];
   showsolution = false;
   solution = null;
 
   ngOnInit(): void {
+  }
+
+  solveProblem(method:String) {
+    if(this.solution == null) {
+      return "Find initial solution first.";
+    }
+    let table = this.table.map( (x)=>x.map(x=>x) );
+    // let weights = this.solution.weights.map( (x)=>x.map(x=>x) );
+    let i,j, u = [...Array(this.supplys.length)], v = [...Array(this.demands.length)];
+    let it = 0;
+    this.solution.eqs = [];
+    this.solution.u = [];
+    this.solution.v = [];
+    this.solution.oppcost = [];
+    let mweights = this.solution.weights.map( (x)=>x.map(x=>x) );
+    this.solution.mweight = [];
+    
+
+    this.solution.eqs.push([]);
+    if(method == 'modi') { //modified distribution method
+
+
+      this.solution.mweight.push(mweights);
+
+      for(i=0;i<table.length;i++) {
+        for(j=0;j<table[0].length;j++) {
+          if(mweights[i][j] != 0) {
+            this.solution.eqs[it].push({i:i, j:j, c:table[i][j]});
+          }
+        }
+      }
+
+      if(this.solution.eqs[it].length < table.length+table[0].length -1) {
+        i=0;j=0;
+        while(mweights[i][j] != 0){
+          j++;
+          if(j >= table[0].length) {
+            j=0;i++;
+          }
+          if(i >= table.length) {
+            console.log("some kind of error bro");
+            return -1;
+          }
+        }
+        this.solution.eqs[it].push({i:i, j:j, c:table[i][j]});
+      }
+
+      let seq = this.solution.eqs[it].map( (x)=>{return {i:x.i,j:x.j,c:x.c}} );
+      let stack = [];
+      stack.push({i:seq[0].i, c: 0});
+
+      // console.log(seq);
+
+      while(stack.length != 0) {
+
+        let eq = stack.pop();
+        if(eq.i != undefined) {
+          u[eq.i] = eq.c;
+          for(i=0;i<seq.length;i++) {
+            if(eq.i == seq[i].i) {
+              stack.push({j:seq[i].j, c: seq[i].c - u[eq.i]});
+              seq[i].j = null;
+            }
+          }
+        } else if(eq.j != undefined) {
+          v[eq.j] = eq.c;
+          for(i=0;i<seq.length;i++) {
+            if(eq.j == seq[i].j) {
+              stack.push({i: seq[i].i, c: seq[i].c - v[eq.j]});
+              seq[i].i = null;
+            }
+          }
+        } else {
+          console.log("some weird error occurred.")
+          break;
+        }
+
+      }
+
+      this.solution.u.push([]);
+      this.solution.v.push([]);
+
+      for(i=0;i<this.supplys.length;i++)
+        this.solution.u[it][i] = u[i];
+      for(i=0;i<this.demands.length;i++)
+        this.solution.v[it][i] = v[i];
+      
+      this.solution.oppcost.push([...Array(mweights.length)].map( (x)=>[...Array(mweights[0].length)].fill(0) ))
+
+      for(i=0;i<this.supplys.length;i++) {
+        for(j=0;j<this.demands.length;j++) {
+          if(mweights[i][j] != 0)
+            continue;
+          this.solution.oppcost[it][i][j] = table[i][j] - (u[i] + v[j]);
+        }
+      }
+
+      it++;
+    }
+
+    ;
   }
 
   findInitialSolution(method:String) {
