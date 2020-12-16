9a5de40b444c6813340a9dc96b8966f5d7db95ed
Mahesh Bansod <mahesh0bansod@gmail.com>
2020-12-16 11:52:43 +0530

    added selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';  
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'numerical-methods';
+  solver:String = 'transportation';
+  
+  setSolver(solver:String) {
+    this.solver = solver;
+  }
 }
